73991470451e19d4a6f4c973151ded0111b68674
Adam Craig and Ashley Maceli <adam@>
2016-04-26 09:36:05 -0700

    add passing integration test for multiple tasks added and displayed

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -40,4 +40,16 @@ public class AppTest extends FluentTest {
     assertThat(pageSource()).contains("Mow the lawn");
   }
 
+  @Test
+  public void multipleTasksAreDisplayedTest() {
+    goTo("http://localhost:4567/");
+    fill("#description").with("Mow the lawn");
+    submit(".btn");
+    click("a", withText("Go Back"));
+    fill("#description").with("Buy groceries");
+    submit(".btn");
+    click("a", withText("Go Back"));
+    assertThat(pageSource()).contains("Mow the lawn");
+    assertThat(pageSource()).contains("Buy groceries");
+  }
 }
