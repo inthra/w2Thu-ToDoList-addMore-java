a8f37b79b94a54708d5b0902573f95dd4168e634
Inthrayuth Mahaphol and Nick Lyman <inthrayuth@>
2016-04-28 09:01:37 -0700

    Pass first BDD test for creating instances of the Category class.

---
+++ src/main/java/Category.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Category {
+  private String mName;
+  private static ArrayList<Category> instances = new ArrayList<Category>();
+  private int mId;
+
+  public Category(String name) {
+    mName = name;
+    instances.add(this);
+    mId = instances.size();
+  }
+
+  // public String getName() {
+  //   return mName;
+  // }
+  //
+  // public static ArrayList<Category> all() {
+  //   return instances;
+  // }
+
+  public static void clear() {
+    instances.clear();
+  }
+
+}
